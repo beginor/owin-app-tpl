fe00c8cf925b4e94934e2ed4cbf7cb5d67aa89c1
beginor <beginor@qq.com>
2017-05-04 10:18:05 +0800

    add attribute route.

--- server/src/OwinApp.Api/Controllers/HomeController.cs
+++ server/src/OwinApp.Api/Controllers/HomeController.cs
@@ -4,8 +4,10 @@ using System.Web.Http;
 
 namespace Beginor.OwinApp.Api.Controllers {
 
+    [RoutePrefix("rest/home")]
     public class HomeController : ApiController {
 
+        [HttpGet]
         public IHttpActionResult Index() {
             return this.Content(HttpStatusCode.OK, "Hello, world!");
         }
