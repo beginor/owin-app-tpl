6ef8f74fb97408757d81f52b469b2c4e5f670446
beginor <beginor@qq.com>
2017-05-05 11:16:59 +0800

    add windsor config

---
+++ server/src/OwinApp.Api/ConsoleLogMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Beginor.OwinApp.Api {
+
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    public class ConsoleLogMiddleware {
+
+        private AppFunc next;
+
+        public void Initialize(AppFunc next) {
+            this.next = next;
+        }
+
+        public async Task Invoke(IDictionary<string, object> env) {
+            await next.Invoke(env);
+            var statusCode = (int)env["owin.ResponseStatusCode"];
+            var color = ConsoleColor.DarkGreen;
+            if (statusCode > 400) {
+                color = ConsoleColor.Red;
+            }
+            Console.ForegroundColor = color;
+            Console.WriteLine("{0} {1} {2}", statusCode, env["owin.RequestMethod"], env["owin.RequestPath"]);
+        }
+
+    }
+
+}
--- server/src/OwinApp.Api/Startup.cs
+++ server/src/OwinApp.Api/Startup.cs
@@ -1,17 +1,85 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 using System.Web.Http;
 using System.Web.Http.Cors;
+using Beginor.Owin.StaticFile;
+using Beginor.Owin.WebApi.Windsor;
+using Beginor.Owin.Windsor;
+using Beginor.OwinApp.Security;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor.Installer;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Logging;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.DataProtection;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using Owin;
 using Swashbuckle.Application;
+
+[assembly: Microsoft.Owin.OwinStartup(typeof(Beginor.OwinApp.Api.Startup))]
 
 namespace Beginor.OwinApp.Api {
 
     public class Startup {
 
         public void Configuration(IAppBuilder app) {
+            // config windsor
+            ConfigWindsor(app);
+            // console log middle ware
+            ConfigConsoleLogMiddleware(app);
+            // owin logger factory
+            ConfigLogger(app);
+            // config static
+            ConfigStaticFile(app);
+            // identity and cookie auth, not used now.
+            ConfigIdentity(app);
+            // config web api
             ConfigWebApi(app);
+        }
+
+        private void ConfigWindsor(IAppBuilder app) {
+            var windsorConfigPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "windsor.config");
+            app.UseWindsorContainer(windsorConfigPath);
+            var container = app.GetWindsorContainer();
+            container.Install(FromAssembly.This());
+            app.UseWindsorMiddleWare();
+        }
+
+        [Conditional("DEBUG")]
+        private void ConfigConsoleLogMiddleware(IAppBuilder app) {
+            app.Use(new ConsoleLogMiddleware());
+        }
+
+        private void ConfigLogger(IAppBuilder app) {
+            app.SetLoggerFactory(
+                app.GetWindsorContainer().Resolve<ILoggerFactory>()
+            );
+        }
+
+        [Conditional("DEBUG")]
+        private void ConfigStaticFile(IAppBuilder app) {
+            app.UseStaticFile(
+                app.GetWindsorContainer().Resolve<StaticFileMiddlewareOptions>()
+            );
+        }
+
+        private void ConfigIdentity(IAppBuilder app) {
+            var container = app.GetWindsorContainer();
+            container.Register(
+                Component.For<IAuthenticationManager>().FromOwinContext().LifestyleTransient()
+            );
+            var dataProtectionProvider = container.Resolve<IDataProtectionProvider>();
+            app.SetDataProtectionProvider(dataProtectionProvider);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                Provider = new AuthenticationProvider(TimeSpan.FromMinutes(30))
+            });
+
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
         }
 
         private void ConfigWebApi(IAppBuilder app) {
@@ -44,7 +112,7 @@ namespace Beginor.OwinApp.Api {
                 routeTemplate: "rest/{controller}/{id}"
             );
             // use windsor container
-            //config.UseWindsorContainer(app.GetWindsorContainer());
+            config.UseWindsorContainer(app.GetWindsorContainer());
             // enable swagger 
             config.EnableSwagger(c => c.SingleApiVersion("v1", "API Help"))
                 .EnableSwaggerUi();
