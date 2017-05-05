c64ecf43db8f6ecc23264e3c613ecdbd8ff063a0
beginor <beginor@qq.com>
2017-05-05 11:16:43 +0800

    add unit test

---
+++ server/test/OwinApp.Test/ConfigTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Http.ExceptionHandling;
+using Beginor.Owin.StaticFile;
+using Microsoft.Owin.Logging;
+using Microsoft.Owin.Security.DataProtection;
+using NUnit.Framework;
+
+namespace Beginor.OwinApp.Test {
+    
+    [TestFixture]
+    public class ConfigTest : WindsorTest {
+        
+        [Test]
+        public void CanResolveLoggerFactory() {
+            var loggerFactory = Container.Resolve<ILoggerFactory>();
+            Assert.NotNull(loggerFactory);
+        }
+
+        [Test]
+        public void CanResolveExceptionLogger() {
+            var exLogger = Container.Resolve<IExceptionLogger>();
+            Assert.NotNull(exLogger);
+        }
+
+        [Test]
+        public void CanResolveAesDataProtectionProvider() {
+            var aes = Container.Resolve<IDataProtectionProvider>();
+            Assert.NotNull(aes);
+        }
+
+        [Test]
+        public void CanResolveStaticFileOptions() {
+            var opts = Container.Resolve<StaticFileMiddlewareOptions>();
+            Assert.NotNull(opts);
+        }
+    }
+}
---
+++ server/test/OwinApp.Test/WindsorTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Beginor.AppFx.Core;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+using NUnit.Framework;
+
+namespace Beginor.OwinApp.Test {
+
+    public abstract class WindsorTest {
+
+        protected IWindsorContainer Container { get; private set; }
+
+        [SetUp]
+        public virtual void Setup() {
+            Container = new WindsorContainer();
+            Container.Register(
+                Component.For<IWindsorContainer>().Instance(Container)
+            );
+            Container.Install(
+                Configuration.FromXmlFile("windsor.config"),
+                FromAssembly.Named("Beginor.OwinApp.Data"),
+                FromAssembly.Named("Beginor.OwinApp.Model"),
+                FromAssembly.Named("Beginor.OwinApp.Logic"),
+                FromAssembly.Named("Beginor.OwinApp.Security"),
+                FromAssembly.Named("Beginor.OwinApp.Api")
+            );
+        }
+
+    }
+
+    public abstract class WindsorTest<TTarget> : WindsorTest {
+
+        protected TTarget Target { get; private set; }
+
+        protected string TargetId { get; }
+
+        public WindsorTest() { }
+
+        public WindsorTest(string targetId) {
+            Argument.NotNullOrEmpty(targetId, nameof(targetId));
+            TargetId = targetId;
+        }
+
+        public override void Setup() {
+            base.Setup();
+            Target = TargetId.IsNullOrEmpty() ? Container.Resolve<TTarget>()
+                : Container.Resolve<TTarget>(TargetId);
+        }
+
+    }
+
+}
